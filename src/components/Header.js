62a66f74134277bceb30c913201df56752bb7b29
luminous3 <githubuser342@gmail.com>
2018-01-30 05:40:24 -0800

    add header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">Welcome</div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
